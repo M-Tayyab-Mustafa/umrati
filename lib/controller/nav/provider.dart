c6cdc4d458996a9556e5e42430a05b0e0cf49c4c
Muhammad Tayyab <m.tayyabmustafa.joiya@gmail.com>
2025-06-01 20:30:21 +0500

    feat: Update location settings and add offset calculation
    This commit introduces functionality to update location settings and calculate offset coordinates.
    
    - Added a function `_setMyLocationToMacca` to update the latitude and longitude of Al-Kaba and Safa Marwa in the settings collection with the user's current location.
    - Implemented `offsetLatLng` function to calculate a new LatLng point given an origin, distance, and bearing. This is used to set the Marwa location relative to Safa.
    - Added `export.dart` import.

--- lib/controller/nav/provider.dart
+++ lib/controller/nav/provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:umrati/export.dart';
 
 import '../../view/nav/umera/tawaf.dart';
 import '../../utils/helper/constants.dart';
@@ -18,10 +19,13 @@ class BottomNavNotifier extends ChangeNotifier {
     selectedTab = selectedOption;
     switch (selectedOption) {
       case BottomNavTabs.profile:
+        _setMyLocationToMacca();
+        break;
       case BottomNavTabs.umera:
         child = const StartTawafPage();
         break;
       case BottomNavTabs.more:
+        break;
       case BottomNavTabs.ziarat:
         child = const ZiaratPage();
         break;
@@ -40,4 +44,31 @@ class BottomNavNotifier extends ChangeNotifier {
     logoAlign = align;
     notifyListeners();
   }
+
+  void _setMyLocationToMacca() async {
+    await Geolocator.requestPermission();
+    var position = await Geolocator.getCurrentPosition();
+    await settingsCollection.doc(CommonDoc.alKaba.name).update({'lat': position.latitude, 'lng': position.longitude});
+    await settingsCollection.doc(CommonDoc.safaMarwa.name).update({'safaLat': position.latitude, 'safaLng': position.longitude});
+    LatLng marwaNewLocation = offsetLatLng(
+      LatLng(position.latitude, position.longitude),
+      double.tryParse((await settingsCollection.doc(CommonDoc.safaMarwa.name).get()).data()!['distance'] as String) ?? 450,
+      0,
+    );
+    await settingsCollection.doc(CommonDoc.safaMarwa.name).update({'marwaLat': marwaNewLocation.latitude, 'marwaLng': marwaNewLocation.longitude});
+  }
+}
+
+LatLng offsetLatLng(LatLng origin, double distanceInMeters, double bearingInDegrees) {
+  const double earthRadius = 6371000;
+  final double bearing = bearingInDegrees * pi / 180;
+
+  final double lat1 = origin.latitude * pi / 180;
+  final double lon1 = origin.longitude * pi / 180;
+
+  final double lat2 = asin(sin(lat1) * cos(distanceInMeters / earthRadius) + cos(lat1) * sin(distanceInMeters / earthRadius) * cos(bearing));
+
+  final double lon2 = lon1 + atan2(sin(bearing) * sin(distanceInMeters / earthRadius) * cos(lat1), cos(distanceInMeters / earthRadius) - sin(lat1) * sin(lat2));
+
+  return LatLng(lat2 * 180 / pi, lon2 * 180 / pi);
 }
